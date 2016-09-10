765b1a4220218cf2e18cbf825c3dbd745f287082
Edelcides Gonçalves <eatg75@gmail.com>
2016-09-10 21:34:29 +0100

    Added support for vcs.

--- cmake/Utils.cmake
+++ cmake/Utils.cmake
@@ -37,7 +37,7 @@ if (CMAKE_BUILD_TYPE STREQUAL "Debug")
   list (APPEND CMAKE_C_FLAGS_DEBUG -Wall -Wextra -Werror -std=c99
                                    -pedantic -Wwrite-strings)
   if (COMPILER_SUPPORTS_SAN)
-    set (SAN_BLACKLIST_FILE "${CMAKE_SOURCE_DIR}/blacklists.txt")
+    set (SAN_BLACKLIST_FILE "${CMAKE_SOURCE_DIR}/res/blacklists.txt")
     list (APPEND CMAKE_C_FLAGS_DEBUG -fPIE )
     list (APPEND CMAKE_EXE_LINKER_FLAGS_DEBUG -fno-omit-frame-pointer
                                               -pie)
